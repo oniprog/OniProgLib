5ac154efd499a3b7a3d2c630aaa4b84591c10912
unknown <tkuman@gmail.com>
2012-12-13 13:33:58 +0000

    ﻿Determinant2x2, 3x3を追加した

---
+++ Library/GeoLib/Math.h
@@ -0,0 +1,29 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief	数学関係
+	@author	oniprog
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace GeoLib {
+
+// 2次の行列式
+template<class _Value>
+_Value	Determinant2x2( const _Value &v1, const _Value &v2, const _Value &v3, const _Value &v4 ) {
+	return v1 * v4 - v2 * v3;
+}
+
+// 3次の行列式
+template<class _Value>
+_Value	Determinant3x3( 
+	const _Value &v1, const _Value &v2, const _Value &v3, 
+	const _Value &v4, const _Value &v5, const _Value &v6, 
+	const _Value &v7, const _Value &v8, const _Value &v9 
+) {
+	return v1 * Determinant2x2( v5, v6, v8, v9) - v2 * Determinant2x2( v4, v6, v7, v9) + v3 * Determinant2x2( v4, v5, v7, v8 );
+}
+
+
+};	// namespace GeoLib
---
+++ UnitTest/Test/GeoLib/TestMath.cpp
@@ -0,0 +1,54 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief	数学関係のテスト
+	@author	oniprog
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include "GeoLib/Math.h"
+#include "MPIR/mpirxx.h"
+
+TEST( Determinant2x2, Test) {
+
+	double dAns = GeoLib::Determinant2x2( 1, 2, 3, 4 );
+	EXPECT_NEAR( -2, dAns, 1E-8 );
+}
+
+TEST( Determinant2x2, TestMPZ) {
+
+	typedef mpz_class Z;
+	double dAns = GeoLib::Determinant2x2( Z(1), Z(2), Z(3), Z(4) ).get_d();
+	EXPECT_NEAR( -2, dAns, 1E-8 );
+}
+
+TEST( Determinant3x3, Test1 ) {
+
+	double dAns = GeoLib::Determinant3x3(1, 0, 0, 0, 1, 2, 0, 3, 4);
+	EXPECT_NEAR( -2, dAns, 1E-8 );
+
+	double dAns2 = GeoLib::Determinant3x3( 0, 1, 0, 1, 0, 2, 3, 0, 4);
+	EXPECT_NEAR( 2, dAns2, 1E-8 );
+
+	double dAns3 = GeoLib::Determinant3x3( 0, 0, 1, 1, 2, 0, 3, 4, 0 );
+	EXPECT_NEAR( -2, dAns3, 1E-8 );
+}
+
+TEST( Determinant3x3, Test2 ) {
+
+	double dAns2 = GeoLib::Determinant3x3( 1, 0, 2, 0, 1, 0, 3, 0, 4);
+	EXPECT_NEAR( -2, dAns2, 1E-8 );
+
+	double dAns3 = GeoLib::Determinant3x3( 0, 1, 2, 1, 0, 0, 0, 3, 4 );
+	EXPECT_NEAR( 2, dAns3, 1E-8 );
+
+	double dAns4 = GeoLib::Determinant3x3( 0, 1, 2, 0, 3, 4, 1, 0, 0 );
+	EXPECT_NEAR( -2, dAns4, 1E-8 );
+}
+
+TEST( Determinant3x3, Test3 ) {
+
+	double dAns = GeoLib::Determinant3x3(1,1,1, 1,1,1, 1,1,1);
+	EXPECT_NEAR(0.0, dAns, 1E-8 );
+}
