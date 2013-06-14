e29af45df8aa22d388e196cef8affbf7e7eab781
U-numerical072\oniprog <tkuman@gmail.com>
2013-06-15 08:32:39 +0900

    ﻿InCircleテストの追加

---
+++ Library/GeoLib/Rational.h
@@ -0,0 +1,39 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief	有理数クラスのラッパー	
+	@author	oniprog
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <limits>
+#include "boost/multiprecision/gmp.hpp"
+
+namespace GeoLib {
+
+// expression templateをOFFにする
+typedef boost::multiprecision::number< boost::multiprecision::gmp_rational, boost::multiprecision::et_off> Rational;
+
+template<class T>
+T static_rational_cast( const Rational &r ) {
+	return r;
+}
+template<class T>
+T static_rational_cast( Rational &&r ) {
+	return r;
+}
+
+// doubleへのCASTを定義する
+template<>
+double static_rational_cast<double>(const Rational&r ) {
+	return mpq_get_d( r.backend().data() );
+}
+
+// doubleへのCASTを定義する
+template<>
+double static_rational_cast<double>( Rational&& r ) {
+	return mpq_get_d( r.backend().data() );
+}
+
+};	// namespace GeoLib
