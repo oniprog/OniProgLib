8bb3c25f64aec97b4f3f777078f2d2bdec537754
Ohkuma <tkuman@gmail.com>
2013-07-27 18:04:18 +0900

    スレッド用スマートポインタを追加した

---
+++ Library/ThreadLib/ThreadSmartPointer.h
@@ -0,0 +1,81 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief	スレッド用 スマートポインタ
+	@author	oniprog
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "boost/shared_ptr.hpp"
+#include "boost/atomic.hpp"
+
+namespace ThreadLib {
+
+template<class T>
+class thread_shared_ptr {
+public:
+    typedef boost::atomic<bool>                         ZObsoleteFlag;
+    typedef boost::shared_ptr<boost::atomic<bool> >     ZSharedObsoleteFlag;
+public:
+
+    thread_shared_ptr() : m_pObsolete(new ZObsoleteFlag()) {}
+    thread_shared_ptr(boost::detail::sp_nullptr_t ) : m_pObsolete(new ZObsoleteFlag()) {}
+
+    // 最初の作成時以外は、使ってはまずい。
+    explicit thread_shared_ptr( T * p ) : m_pP(boost::shared_ptr<T>(p)), m_pObsolete(new ZObsoleteFlag()) {}
+    explicit thread_shared_ptr( const T * p ) : m_pP(boost::shared_ptr<T>(p)), m_pObsolete(new ZObsoleteFlag()) {}
+
+    // 作成用。ここから保護が始まる。
+    thread_shared_ptr( boost::shared_ptr<T> &p ) : m_pP(p), m_pObsolete( new ZObsoleteFlag() ) {}
+    thread_shared_ptr( const boost::shared_ptr<T> &p ) : m_pP(p), m_pObsolete( new ZObsoleteFlag() ) {}
+
+    thread_shared_ptr( const thread_shared_ptr<T> &m ) : m_pP(m.m_pP), m_pObsolete(m.m_pObsolete) {
+    }
+
+    thread_shared_ptr &operator = ( const thread_shared_ptr &m ) {
+
+        // Obsolete属性を付ける
+        SetObsolete();
+
+        // コピーする
+        m_pP = m.m_pP;
+        m_pObsolete = m.m_pObsolete;
+        return *this;
+    }
+
+    // このスマートポインタが無効になっているかをチェックする
+    bool    IsObsolete() const {
+        return m_pObsolete->load();
+    }
+
+    // このスマートポインタが無効であることをセットする(セットだけ)
+    void    SetObsolete() {
+        m_pObsolete->store(true);
+    }
+
+    // const参照しか用意しない
+    const T * operator->() {
+        return &*m_pP;
+    } 
+    const T & operator*() {
+        return *m_pP;
+    }
+
+    // 書き込み用に作成する
+    boost::shared_ptr<T>   Write() {
+        // CopyInsideで中身をコピーしてそのポインターを返すこと
+        return boost::shared_ptr<T>( ThreadLib::CopyInside( m_pP.get() ) );
+    }
+
+private:
+    ZSharedObsoleteFlag             m_pObsolete;        ///< 破棄済みフラグ
+    boost::shared_ptr<T>            m_pP;               ///< ポインタを持つ
+};
+
+// 内部コピー用の関数を定義する
+template<class T>
+T* CopyInside( const T* );
+
+
+};	// namespace ThreadLib
---
+++ UnitTest/Test/ThreadLib/TestThreadSmartPointer.cpp
@@ -0,0 +1,125 @@
+﻿///////////////////////////////////////////////////////////////////////////////////
+/** @file
+	@brief	スレッド用スマートポインタのテスト
+	@author	oniprog
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "gtest/gtest.h"
+
+#include <windows.h>
+
+#include "boost/thread.hpp"
+#include "ThreadLib/ThreadSmartPointer.h"
+
+///////////////////////////////////////////////////////////////////////////////////
+struct Dummy {
+
+    Dummy()  { IncAtomic(nCntConstructor); };
+    ~Dummy() { IncAtomic(nCntDestroy); };
+    Dummy( const Dummy &dm) {
+        IncAtomic(nCntConstructor);
+    }
+
+    template<class T>
+    void IncAtomic( T & v ) {
+        while(true) {
+            int val = v.load();
+            if ( v.compare_exchange_strong( val, val+1 ) )
+                break;
+        }
+    }
+
+    static void    Reset() {
+        nCntConstructor.store(0);
+        nCntDestroy.store(0);
+    }
+
+    static void GetResult( int &nCnt1, int &nCnt2 ) {
+        nCnt1 = nCntConstructor.load();
+        nCnt2 = nCntDestroy.load();
+    }
+
+    int nDummy;
+
+    static boost::atomic<int>      nCntConstructor;
+    static boost::atomic<int>      nCntDestroy;
+};
+
+boost::atomic<int> Dummy::nCntConstructor;
+boost::atomic<int> Dummy::nCntDestroy;
+
+///////////////////////////////////////////////////////////////////////////////////
+namespace ThreadLib {
+
+// Dummyをコピーして返す
+template<>
+Dummy *CopyInside(const Dummy *pP) {
+    return new Dummy( *pP );
+}
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+/*
+    何らかの処理をするスレッド。しかし、pA2がObsoleteになったら処理を終了する。
+    本来は、Obsoleteになる前に処理が完了したら、そのまま継続するようにする。
+*/
+static void Thread2( ThreadLib::thread_shared_ptr<Dummy> pA2 ) {
+
+    while(true) {
+
+        if ( pA2.IsObsolete() )
+            break;
+
+        // pA2の内容を読みだして、いろいろと処理をすることを想定している。
+        // pA2は読み出し専用
+
+        pA2->nDummy;
+        //pA2->nDummy++;        // 変更はできない
+
+        ::Sleep(100);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+static void Thread1( ThreadLib::thread_shared_ptr<Dummy> pA1 ) {
+
+    // pA1を使って何らかの作業をおこなうスレッド
+    std::vector<boost::thread> listThread;
+    for(int it=0; it<10; ++it) {
+
+        // スレッド2にて、pA1を使って処理をさせる
+        boost::thread thread2( &Thread2, pA1 );
+        listThread.push_back(std::move(thread2));
+
+        // しかし、このスレッドで何か処理をして上書きがしたくなった
+        auto pA2 = pA1.Write(); 
+
+        Sleep(100);
+
+        // 上書きする。これでpA1はObsoleteになる。
+        pA1 = pA2;
+    }
+
+    // 終了待ち
+    std::for_each( listThread.begin(), listThread.end(), [&](boost::thread &th) {th.join(); } );
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+TEST(ThreadSmartPointer, Test1) {
+
+    {
+        Dummy::Reset();
+
+        ThreadLib::thread_shared_ptr<Dummy> pA1( new Dummy() );
+
+        boost::thread thread1( &Thread1, pA1 );
+        thread1.join();
+    }
+
+    // コンストラクタとデストラクタが呼ばれた回数をチェックする
+    int nCnt1, nCnt2;
+    Dummy::GetResult(nCnt1, nCnt2);
+    EXPECT_EQ( 11, nCnt1 );
+    EXPECT_EQ( 11, nCnt2 );
+}
